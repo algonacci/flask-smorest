e3ae83c65e7d9b147150f4afc4f0ea4ae6f255df
algonacci <algonacci1@gmail.com>
2022-10-09 00:02:16 +0700

    install flask-smorest

--- app.py
+++ app.py
@@ -1,18 +1,9 @@
 from flask import Flask, jsonify, request, render_template
+from flask_smorest import abort
+from db import items, stores
+import uuid
 
 app = Flask(__name__)
-
-stores = [
-    {
-        'name': 'My Wonderful Store',
-        'items': [
-            {
-                'name': 'My Item',
-                'price': 15.99
-            }
-        ]
-    }
-]
 
 
 @app.route('/')
@@ -22,47 +13,48 @@ def index():
 
 @app.route('/store', methods=['POST'])
 def create_store():
-    request_data = request.get_json()
-    new_store = {
-        'name': request_data['name'],
-        'items': []
-    }
-    stores.append(new_store)
-    return jsonify(new_store)
+    store_data = request.get_json()
+    store_id = uuid.uuid4().hex
+    store = {**store_data, "id": store_id}
+    stores[store_id] = store
+    return store, 201
 
 
-@app.route('/store/<string:name>')
-def get_store(name):
-    for store in stores:
-        if store['name'] == name:
-            return jsonify(store)
-    return jsonify({'message': 'store not found', 'status_code': 404})
+@app.route('/store/<string:store_id>')
+def get_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        return abort(404, message="store not found")
 
 
 @app.route('/store')
 def get_stores():
-    return jsonify({'stores': stores})
+    return {'stores': list(stores.values())}
 
 
-@app.route('/store/<string:name>/item', methods=['POST'])
-def create_item_in_store(name):
-    request_data = request.get_json()
-    for store in stores:
-        if store['name'] == name:
-            new_item = {
-                'name': request_data['name'],
-                'price': request_data['price']
-            }
-            store['items'].append(new_item)
-            return jsonify(new_item)
-    return jsonify({'message': 'store not found', 'status_code': 404})
+@app.route('/item', methods=['POST'])
+def create_item():
+    item_data = request.get_json()
+    if item_data["store_id"] not in stores:
+        abort(404, message="item not found")
+
+    item_id = uuid.uuid4().hex
+    item = {**item_data, "id": item_id}
+    return item, 201
 
 
-@app.route('/store/<string:name>/item')
-def get_item_in_store(name):
-    for store in stores:
-        if store['name'] == name:
-            return jsonify({'items': store['items']})
+@app.get("/item")
+def get_all_items():
+    return {"items": list(items.values())}
+
+
+@app.route('/item/<string:item_id>')
+def get_item(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        abort(404, message="item not found")
 
 
 if __name__ == '__main__':
