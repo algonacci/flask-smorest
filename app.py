dd0352db4b46d915f422fc111c0ac0398a0ee139
algonacci <algonacci1@gmail.com>
2022-10-08 23:28:19 +0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,69 @@
+from flask import Flask, jsonify, request, render_template
+
+app = Flask(__name__)
+
+stores = [
+    {
+        'name': 'My Wonderful Store',
+        'items': [
+            {
+                'name': 'My Item',
+                'price': 15.99
+            }
+        ]
+    }
+]
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/store', methods=['POST'])
+def create_store():
+    request_data = request.get_json()
+    new_store = {
+        'name': request_data['name'],
+        'items': []
+    }
+    stores.append(new_store)
+    return jsonify(new_store)
+
+
+@app.route('/store/<string:name>')
+def get_store(name):
+    for store in stores:
+        if store['name'] == name:
+            return jsonify(store)
+    return jsonify({'message': 'store not found', 'status_code': 404})
+
+
+@app.route('/store')
+def get_stores():
+    return jsonify({'stores': stores})
+
+
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store['name'] == name:
+            new_item = {
+                'name': request_data['name'],
+                'price': request_data['price']
+            }
+            store['items'].append(new_item)
+            return jsonify(new_item)
+    return jsonify({'message': 'store not found', 'status_code': 404})
+
+
+@app.route('/store/<string:name>/item')
+def get_item_in_store(name):
+    for store in stores:
+        if store['name'] == name:
+            return jsonify({'items': store['items']})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
